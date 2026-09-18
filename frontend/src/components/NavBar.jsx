4b1a7892e7ac31bce871ff733860fbf78790dae2
dinopatates <gustavecheval819@gmail.com>
2026-09-18 09:42:10 +0200

    preparing some components for use. Navbar and Layout

---
+++ frontend/src/components/NavBar.jsx
@@ -0,0 +1,24 @@
+export default function NavBar() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <a href="/">Home</a>
+                </li>
+                <li>
+                    <a href="/vehicles">Vehicles</a>
+                </li>
+                <li>
+                    <a href="/details">Details</a>
+
+                </li>
+                <li> 
+                    <a href="/about-us">About Us</a>
+                </li>
+                <li>
+                    <a href="/contact">Contact Us</a>
+                </li>
+            </ul>
+        </nav>
+    );
+}
