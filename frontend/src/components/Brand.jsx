385b1996bae73f352f7b746b59d82ada64ef6d55
dinopatates <gustavecheval819@gmail.com>
2026-09-24 16:30:28 +0200

    fix(backend) : can't use symfony because of 1 WEEK CONSTRAIN

---
+++ frontend/src/components/Brand.jsx
@@ -0,0 +1,7 @@
+export default function Brand() {
+    return (
+        <div className="text-2xl font-bold">
+            RentABike
+        </div>
+    );
+}
---
+++ frontend/src/components/MiniContact.jsx
@@ -0,0 +1,8 @@
+export default function MiniContact() {
+    return (
+        <div className="flex flex-col gap-2">
+            <p>Need help?</p>
+            <p>+33 6 12 34 56 78</p>
+        </div>
+    )
+}
